0440b1fb5c201e0968ab692ce54f1292e9585a36
TheOnly8Z <888888zombies@gmail.com>
2021-10-09 23:22:21 -0400

    remove redundant stat

--- lua/arccw/shared/attachments/uc_tp_overload.lua
+++ lua/arccw/shared/attachments/uc_tp_overload.lua
@@ -3,7 +3,6 @@ att.PrintName = "Overload"
 att.Icon = Material("entities/att/arccw_uc_tp_overload.png", "smooth mips")
 att.Description = "With a little well-placed grease and some physical force, most magazines can be made to accept an extra round."
 att.Desc_Pros = {
-    "uc.overload"
 }
 att.Desc_Cons = {
 }
