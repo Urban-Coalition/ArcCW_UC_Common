8a50f1820dfa589e961cf7b581645a429b216fd6
TheOnly8Z <888888zombies@gmail.com>
2021-10-31 16:56:17 -0400

    improved sealed bolt

--- lua/arccw/shared/attachments/uc_fg_underwater.lua
+++ lua/arccw/shared/attachments/uc_fg_underwater.lua
@@ -1,7 +1,7 @@
 att.PrintName = "Sealed Bolt"
 
-att.Icon = nil -- Material("entities/att/acwatt_lowpolysaiga12extmag.png", "smooth mips")
-att.Description = "A set of modifications needed to allow the weapon to fire underwater."
+att.Icon = Material("entities/att/acwatt_uc_sealedbolt.png", "smooth mips")
+att.Description = "Watertight modifications that allow the weapon to fire underwater."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -13,3 +13,21 @@ att.AutoStats = true
 att.SortOrder = 3
 
 att.Override_CanFireUnderwater = true
+
+att.Hook_AddShootSound = function(wep)
+    if wep:GetOwner():WaterLevel() >= 3 then
+        wep:MyEmitSound("weapons/underwater_explode" .. math.random(3, 4) .. ".wav", 70, math.random(60, 80), 0.5, CHAN_AUTO)
+    end
+end
+
+att.Hook_PreDoEffects = function(wep)
+    if wep:GetOwner():WaterLevel() >= 3 then
+        return true
+    end
+end
+
+att.M_Hook_Mult_ShootPitch = function(wep, data)
+    if wep:GetOwner():WaterLevel() >= 3 then
+        data.mult = data.mult * 0.6
+    end
+end
