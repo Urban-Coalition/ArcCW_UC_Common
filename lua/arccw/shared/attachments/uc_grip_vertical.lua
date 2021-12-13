05d211bea717d3255db12efc330010b3658b6ad9
Sninctbur <hedpub@protonmail.com>
2021-12-12 21:48:48 -0600

    Unfinished and untested vertical foregrip port

---
+++ lua/arccw/shared/attachments/uc_grip_vertical.lua
@@ -0,0 +1,11 @@
+att.PrintName = "Vertical Forward Grip"
+att.AbbrevName = "Vertical Foregrip"
+att.Icon = nil -- todo
+att.Description = "Decreases recoil by providing a more secure holding position."
+
+att.AutoStats = true
+att.Slot = "foregrip"
+
+att.Mult_Recoil = .8
+att.Mult_SightTime = 1.15
+att.Mult_MoveDispersion = 1.25
