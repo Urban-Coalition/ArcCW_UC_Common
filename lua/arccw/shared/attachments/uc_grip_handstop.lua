fb950ee93f0089060046dd680dff33433f3e43ee
1darky <95997959+1darky@users.noreply.github.com>
2021-12-22 21:52:53 +0700

    hansdtop balance (i m bad at balance)

--- lua/arccw/shared/attachments/uc_grip_handstop.lua
+++ lua/arccw/shared/attachments/uc_grip_handstop.lua
@@ -1,7 +1,7 @@
 att.PrintName = "UC Handstop"
 att.AbbrevName = "UC Handstop"
 att.Icon = nil -- todo
-att.Description = "c  clamp"
+att.Description = "yo better use it without moving"
 
 att.AutoStats = true
 att.Slot = "foregrip"
@@ -14,6 +14,11 @@ att.ModelSkin = 1
 att.Override_ActivePos = Vector(-0.5, -0.5, 0.5)
 att.Override_ActiveAng = Angle(0, 0, -7)
 
-att.Mult_Recoil = .8
-att.Mult_SightTime = 1.15
-att.Mult_MoveDispersion = 1.25
+att.Mult_Recoil = 0.2
+att.Mult_RecoilSide = 2
+att.Mult_MoveDispersion = 1.9
+att.Mult_MoveSpeed = 0.85
+att.Mult_Sway = 0.75
+att.Mult_SightedSpeedMult = 0.5
+att.Mult_DrawTime = 1.4
+att.Mult_SightTime = 0.8
