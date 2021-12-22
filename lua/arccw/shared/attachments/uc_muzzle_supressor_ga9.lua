a4796304005b31c324e1c90dd57c8b3fcaf5ad6d
Sninctbur <hedpub@protonmail.com>
2021-12-22 17:00:00 -0600

    this fucking glock

--- lua/arccw/shared/attachments/uc_muzzle_supressor_ga9.lua
+++ lua/arccw/shared/attachments/uc_muzzle_supressor_ga9.lua
@@ -13,7 +13,7 @@ att.Slot = {"muzzle"}
 att.SortOrder = 150
 
 att.Model = "models/weapons/arccw/atts/uc_ga_revolution9.mdl"
-att.ModelOffset = Vector(2.7, 0, 0)
+att.ModelOffset = Vector(2.2, 0, 0)
 att.ModelScale = Vector(1.15, 1.15, 1.15)
 att.OffsetAng = Angle(0, 0, 0)
 
