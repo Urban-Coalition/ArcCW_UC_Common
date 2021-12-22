9a46182746e419566c57eed06bb150610f3f0eb2
1darky <95997959+1darky@users.noreply.github.com>
2021-12-22 21:16:11 +0700

    muzzle stuff (brakes etc) lua
    configuure

---
+++ lua/arccw/shared/attachments/uc_grip_handstop.lua
@@ -0,0 +1,19 @@
+att.PrintName = "UC Handstop"
+att.AbbrevName = "UC Handstop"
+att.Icon = nil -- todo
+att.Description = "c  clamp"
+
+att.AutoStats = true
+att.Slot = "foregrip"
+
+att.LHIK = true
+
+att.ModelOffset = Vector(2, 0, -0.8)
+att.Model = "models/weapons/arccw/atts/uc_handstop.mdl"
+att.ModelSkin = 1
+att.Override_ActivePos = Vector(-0.5, -0.5, 0.5)
+att.Override_ActiveAng = Angle(0, 0, -7)
+
+att.Mult_Recoil = .8
+att.Mult_SightTime = 1.15
+att.Mult_MoveDispersion = 1.25
---
+++ lua/arccw/shared/attachments/uc_muzzle_compensator.lua
@@ -0,0 +1,32 @@
+att.PrintName = "idk Compensator" -- fictional
+att.AbbrevName = "idk Compensator"
+att.Icon = nil
+att.Description = "nigga compressed"
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.AutoStats = true
+att.Slot = {"muzzle"}
+
+att.SortOrder = 150
+
+att.Model = "models/weapons/arccw/atts/uc_muzzle5.mdl"
+att.ModelOffset = Vector(2.2, 0, 0)
+att.ModelScale = Vector(1.15, 1.15, 1.15)
+att.OffsetAng = Angle(0, 0, 0)
+
+-- att.Silencer = true
+-- att.Override_MuzzleEffect = "muzzleflash_suppressed"
+att.IsMuzzleDevice = true
+
+-- att.Mult_ShootPitch = 1.1
+-- att.Mult_ShootVol = 0.75
+-- att.Mult_Range = 1.1
+
+att.Add_BarrelLength = 2
+-- att.Mult_SightTime = 1.15
+-- att.Mult_Sway = 1.25
+
+att.AttachSound = "arccw_uc/common/gunsmith/suppressor_thread.ogg"
