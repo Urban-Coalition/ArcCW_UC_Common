134fec19423f4374b77e9667f5c1ff2f471eff3b
1darky <95997959+1darky@users.noreply.github.com>
2021-12-30 15:12:57 +0700

    psssso

---
+++ lua/arccw/shared/attachments/uc_optic_pso1.lua
@@ -0,0 +1,50 @@
+att.PrintName = "PSO-1 (4x)"
+att.AbbrevName = "PSO-1 (4x)"
+att.Icon = nil
+att.Description = "Medium range combat scope for improved precision at longer ranges."
+
+att.SortOrder = 300
+
+att.Desc_Pros = {
+    "autostat.holosight",
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = {"ur_ak_optic"}
+
+att.Model = "models/weapons/arccw/atts/ur_pso1.mdl"
+att.ModelOffset = Vector(-2, 0, -4.55)
+
+att.AdditionalSights = {
+    {
+        Pos = Vector(0, 13, -1.5),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.1,
+        ScrollFunc = ArcCW.SCROLL_ZOOM,
+        ScopeMagnificationMin = 1.5,
+        ScopeMagnificationMax = 3,
+        ScopeMagnification = 3,
+        HolosightData = {
+            Holosight = true,
+            HolosightReticle = Material("hud/scopes/uc_pso.png", "mips smooth"),
+            HolosightNoFlare = true,
+            HolosightSize = 10.5,
+            HolosightPiece = "models/weapons/arccw/atts/ur_pso1_hsp.mdl",
+            HolosightBlackbox = true,
+            HolosightMagnification = 3,
+            Colorable = true,
+        },
+    }
+}
+
+-- att.Holosight = true
+-- att.HolosightReticle = Material("mifl_tarkov_reticle/dot.png", "mips smooth")
+
+att.HolosightPiece = "models/weapons/arccw/atts/ur_pso1_hsp.mdl"
+-- att.HolosightNoFlare = true
+-- att.HolosightSize = 1
+-- att.HolosightBone = "holosight"
+att.Colorable = true
+
+att.Mult_SightedSpeedMult = .8
