43ac5cbe71f8ad922c7b8ea1fbcdc8ab5af665ad
Sninctbur <hedpub@protonmail.com>
2021-12-30 02:36:37 -0600

    Russian suppressors implemented in a horrific state

---
+++ lua/arccw/shared/attachments/uc_muzzle_supressor_pbs4.lua
@@ -0,0 +1,32 @@
+att.PrintName = "PBS-4 Suppressor"
+
+att.Icon = Material("entities/att/acwatt_uc_supressor_light.png", "mips smooth")
+att.Description = "Traps propellant gases from the muzzle, hiding muzzle flash and reducing volume significantly.\nRussian-made. Stats are temporary."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.AutoStats = true
+att.Slot = {"muzzle"}
+
+att.SortOrder = 150
+
+att.Model = "models/weapons/arccw/atts/uc_pbs4.mdl"
+att.ModelOffset = Vector(.8, 0.1, 0)
+att.OffsetAng = Angle(0, 180, 0)
+
+att.Silencer = true
+att.Override_MuzzleEffect = "muzzleflash_suppressed"
+att.IsMuzzleDevice = true
+
+att.Mult_ShootPitch = 1.1
+att.Mult_ShootVol = 0.75
+att.Mult_Range = 0.85
+att.Mult_RangeMin = 1.25
+
+att.Add_BarrelLength = 5
+att.Mult_SightTime = 1.15
+att.Mult_Sway = 1.15
+
+att.AttachSound = "arccw_uc/common/gunsmith/suppressor_thread.ogg"
