0db4f53431d929a3b908a246d1c62a8eba9da842
1darky <95997959+1darky@users.noreply.github.com>
2022-01-05 16:31:53 +0700

    rzentjh yours grips are here right now in game

---
+++ lua/arccw/shared/attachments/uc_grip_troy.lua
@@ -0,0 +1,19 @@
+att.PrintName = "Troy Modular Combat Grip"
+att.AbbrevName = "Troy Grip"
+att.Icon = nil -- todo
+att.Description = "wrist strain"
+
+att.AutoStats = true
+att.Slot = "foregrip"
+
+att.LHIK = true
+
+att.ModelOffset = Vector(0, 0, -0.7)
+att.Model = "models/weapons/arccw/atts/uc_troygrip.mdl"
+-- att.ModelSkin  = 1
+
+att.Mult_SightTime = .85
+att.Mult_Sway = 1.5
+att.Mult_Recoil = 1.05
+
+-- att.Ignore = true
