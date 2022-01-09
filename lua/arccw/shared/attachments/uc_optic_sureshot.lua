76bb6aee5edfb9b6a8a6540147340998904e3ab0
Sninctbur <hedpub@protonmail.com>
2022-01-08 23:55:14 -0600

    Sureshot pos/scale adjustments

--- lua/arccw/shared/attachments/uc_optic_sureshot.lua
+++ lua/arccw/shared/attachments/uc_optic_sureshot.lua
@@ -15,9 +15,12 @@ att.Slot = {"optic"}
 
 att.Model = "models/weapons/arccw/atts/uc_sureshot.mdl"
 
+att.ModelOffset = Vector(0,0,.2)
+att.ModelScale = Vector(1.1,1.1,1.1)
+
 att.AdditionalSights = {
     {
-        Pos = Vector(0, 12, -1.375),
+        Pos = Vector(0, 12, -1.7),
         Ang = Angle(0, 0, 0),
         Magnification = 1.1,
         ScrollFunc = ArcCW.SCROLL_NONE
