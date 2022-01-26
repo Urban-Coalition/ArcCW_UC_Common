58512f5afc7e83f6d8c42bb753e249dd6809855f
TheOnly8Z <888888zombies@gmail.com>
2022-01-25 20:49:05 -0500

    tweak afg2

--- lua/arccw/shared/attachments/uc_grip_magpul_afg2.lua
+++ lua/arccw/shared/attachments/uc_grip_magpul_afg2.lua
@@ -13,6 +13,7 @@ att.ModelOffset = Vector(0, 0, -0.75)
 att.Model = "models/weapons/arccw/atts/uc_magpul_afg2.mdl"
 att.ModelSkin  = 1
 
-att.Mult_SightTime = .85
-att.Mult_Sway = 1.5
-att.Mult_Recoil = 1.05
+att.Mult_SightTime = 0.8
+att.Mult_Sway = 1.25
+att.Mult_Recoil = 1.1
+att.Mult_RecoilSide = 1.25
--- lua/arccw/shared/attachments/uc_grip_magpul_afg2_tan.lua
+++ lua/arccw/shared/attachments/uc_grip_magpul_afg2_tan.lua
@@ -1,7 +1,7 @@
 att.PrintName = "MAGPUL AFG-2 Angled Fore Grip (Tan)"
 att.AbbrevName = "Angled Foregrip (Tan)"
 att.Icon = nil -- todo
-att.Description = "An ergonomically angled grip surface reduces wrist strain, increasing maneuverability.\nThis unit is cosmetically colored tan."
+att.Description = "An ergonomically angled grip surface reduces wrist strain, increasing maneuverability but weakening grip stability.\nThis unit is cosmetically colored tan."
 
 att.AutoStats = true
 att.Slot = "foregrip"
@@ -12,6 +12,7 @@ att.ModelOffset = Vector(0, 0, -0.75)
 
 att.Model = "models/weapons/arccw/atts/uc_magpul_afg2.mdl"
 
-att.Mult_SightTime = .85
-att.Mult_Sway = 1.5
-att.Mult_Recoil = 1.05
+att.Mult_SightTime = 0.8
+att.Mult_Sway = 1.25
+att.Mult_Recoil = 1.1
+att.Mult_RecoilSide = 1.25
