42acbf4e0afb8edfc41da6da548f4a236b780ae6
1darky <95997959+1darky@users.noreply.github.com>
2022-02-21 02:16:27 +0700

    real nvise

--- lua/arccw/shared/attachments/uc_optic_nvis.lua
+++ lua/arccw/shared/attachments/uc_optic_nvis.lua
@@ -24,7 +24,7 @@ att.ModelOffset = Vector(0, 0, 0.18)
 
 att.AdditionalSights = {
     {
-        Pos = Vector(-0.035, 8, -1.25),
+        Pos = Vector(-0.035, 7, -1.25),
         Ang = Angle(0, 0, 0),
         Magnification = 1.1,
         ScrollFunc = ArcCW.SCROLL_ZOOM,
@@ -42,6 +42,8 @@ att.AdditionalSights = {
         IgnoreExtra = true,
         Contrast = 0.51,
         Brightness = 0.1,
+        ForceLowRes = true,
+        FPSLock = 42,
         -- SpecialScopeFunction = function(screen)
             -- render.PushRenderTarget(screen)
             
@@ -53,7 +55,7 @@ att.AdditionalSights = {
         -- end,
     },
     {
-        Pos = Vector(-0.035, 8, -1.25),
+        Pos = Vector(-0.035, 7, -1.25),
         Ang = Angle(0, 0, 0),
         Magnification = 1.1,
         ScrollFunc = ArcCW.SCROLL_ZOOM,
@@ -71,6 +73,8 @@ att.AdditionalSights = {
         IgnoreExtra = true,
         Contrast = 0.7,
         Brightness = 0.5,
+        ForceLowRes = true,
+        FPSLock = 42,
         -- SpecialScopeFunction = function(screen)
             -- render.PushRenderTarget(screen)
             
@@ -86,9 +90,9 @@ att.AdditionalSights = {
 att.ScopeGlint = true
 
 att.Holosight = true
-att.HolosightReticle = Material("hud/scopes/uc_nvis_reticle1.png", "mips smooth")
+att.HolosightReticle = Material("hud/scopes/uc_nvis_reticle1grid.png", "mips smooth")
 att.HolosightNoFlare = true
-att.HolosightSize = 9.2
+att.HolosightSize = 12
 att.HolosightBone = "holosight"
 att.HolosightPiece = "models/weapons/arccw/atts/uc_nvis_hsp.mdl"
 att.Colorable = false
@@ -101,19 +105,3 @@ att.Mult_SightTime = 1.25
 att.Mult_SightedSpeedMult = 0.65
 att.Mult_SpeedMult = 0.9
 att.Mult_Sway = 1.5
-
-local Ret1 = Material("hud/scopes/uc_nvis_reticle1.png", "mips smooth")
-local Ret2 = Material("hud/scopes/uc_nvis_reticle2.png", "mips smooth")
-
-att.HolosightReticle = Ret1
-
-att.ToggleStats = {
-    {
-        PrintName = "Three Lines Reticle",
-        HolosightReticle = Ret1
-    },
-    {
-        PrintName = "Dot Reticle",
-        HolosightReticle = Ret2
-    },
-}
