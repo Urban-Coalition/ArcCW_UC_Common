a77ff91c94784cb3e0c10674b3bb3d822d6ccb94
Sninctbur <hedpub@protonmail.com>
2022-02-26 19:43:49 -0600

    DRGN rebalance

--- lua/arccw/shared/attachments/uc_ammo_sg_drgn.lua
+++ lua/arccw/shared/attachments/uc_ammo_sg_drgn.lua
@@ -2,14 +2,17 @@ att.PrintName = "\"DRGN\" Dragon's Breath"
 
 att.SortOrder = -1
 att.Icon = Material("entities/att/arccw_uc_ammo_shotgun_generic.png", "mips smooth")
-att.Description = [[Novelty round that creates a spectacular, pyrotechnic flare of magnesium pellets.
-Burning at over 3,000 °F, the blast produces enough heat to set objects and people ablaze.]]
+att.Description = [[Novelty magnesium-based round that projects a spectacular jet of sparks and fire.
+Burning at over 3,000 °F, the blast isn't immediately lethal, but can easily set objects and people ablaze.
+
+Only compatible with manual action shotguns due to a weak pressure curve.]]
 att.Desc_Pros = {
     "pro.ignite",
     "uc.pellet.24"
 }
 att.Desc_Cons = {
-    "uc.dragon"
+    "uc.dragon",
+    "uc.alwaysphys",
 }
 att.Desc_Neutrals = {
 }
@@ -17,33 +20,43 @@ att.Slot = "ud_ammo_shotgun"
 
 att.AutoStats = true
 
-att.Override_DamageType = DMG_BURN
+--att.Override_DamageType = DMG_BURN
 att.Add_Num = 24
 
-att.Hook_PostBulletHit = function(wep,data)
-    if SERVER and data.tr.Entity then
-        local dur = 5 - (data.range * 2) / wep.Range
-        
-        if dur > 0 then
-            data.tr.Entity:Ignite(dur)
-        end
-    end
-end
+-- att.Hook_PostBulletHit = function(wep,data)
+--     if SERVER and data.tr.Entity then
+--         local dur = 6 - (data.range * 2) / wep.Range
+--         if dur > 0 then
+--             data.tr.Entity:Ignite(dur)
+--         end
+--     end
+-- end
 
 att.Hook_PhysBulletHit = function(wep,data)
     if SERVER and data.tr.Entity then
-        local dur = 5 - (data.bullet.Travelled * 2 * ArcCW.HUToM) / wep.Range
+        local dur = 6 - (data.bullet.Travelled * 2 * ArcCW.HUToM) / wep.Range
         if dur > 0 then
+            data.tr.Entity:Extinguish() -- Entity:Ignite() doesn't work if it's already on fire, gotta prevent that DPS loss
             data.tr.Entity:Ignite(dur)
         end
     end
+
+    local effect = EffectData()
+    effect:SetOrigin(data.tr.HitPos)
+    util.Effect("StunstickImpact",effect)
+    -- if data.tr.HitWorld and math.random(1,3) == 3 then
+    --     util.Decal("FadingScorch",data.tr.HitPos - data.tr.HitNormal,data.tr.HitPos + data.tr.HitNormal) -- this is too spammy
+    -- end
 end
 
 att.Mult_AccuracyMOA = 3
 att.Mult_Damage = .5
 att.Mult_DamageMin = .5
-att.Mult_Range = .5
 att.Mult_Penetration = .5
+att.Mult_Range = .35
+
+att.Override_AlwaysPhysBullet = true
+att.Mult_PhysBulletGravity = 3
 
 --att.Override_PhysTracerProfile = 1
 att.Override_MuzzleEffect = "muzzleflash_dragonbreath"
