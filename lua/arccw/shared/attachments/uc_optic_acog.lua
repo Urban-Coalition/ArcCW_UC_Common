5fad5e8cd582ca36d3bfafdba1c2bc1544260c6d
Task <93636955+Taskmer@users.noreply.github.com>
2022-03-03 13:43:04 +1100

    Minor description changes (reelism)

--- lua/arccw/shared/attachments/uc_optic_acog.lua
+++ lua/arccw/shared/attachments/uc_optic_acog.lua
@@ -1,7 +1,7 @@
 att.PrintName = "Trijicon Advanced Combat Optical Gunsight (4x)"
 att.AbbrevName = "ACOG (4x)"
 att.Icon = Material("entities/att/acwatt_uc_optic_acog.png", "mips smooth")
-att.Description = "Medium range combat scope for improved precision at longer ranges.\nEquipped with backup iron sights for use in emergencies."
+att.Description = "Medium range combat optic for improved precision at longer ranges.\nEquipped with backup iron sights for use in emergencies."
 
 att.SortOrder = 4
 
