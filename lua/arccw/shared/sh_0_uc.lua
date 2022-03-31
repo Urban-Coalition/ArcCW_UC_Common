cdbc00d6577d4b60f4594078993ea5b41f74fd94
Taskmer <93636955+Taskmer@users.noreply.github.com>
2022-04-01 10:01:36 +1100

    Added velocity to ballistics table

--- lua/arccw/shared/sh_0_uc.lua
+++ lua/arccw/shared/sh_0_uc.lua
@@ -63,124 +63,147 @@ ArcCW.UC.StdDmg = {
     ["22lr"] = {
         max = 12,
         min = 7,
-        pen = 3
+        pen = 3,
+        vel = 330
     },
     ["9mm"] = {
         max = 30,
         min = 17,
-        pen = 6
+        pen = 6,
+        vel = 380
     },
     ["10mm"] = {
         max = 35,
         min = 20,
-        pen = 8
+        pen = 8,
+        vel = 400
     },
     ["380acp"] = {
         max = 30,
         min = 15,
-        pen = 3
+        pen = 3,
+        vel = 390
     },
     ["40sw"] = {
         max = 30,
         min = 23,
-        pen = 8
+        pen = 8,
+        vel = 340
     },
     ["45acp"] = {
         max = 45,
         min = 15,
-        pen = 9
+        pen = 9,
+        vel = 253
     },
     ["357sig"] = {
         max = 33,
         min = 17,
-        pen = 6
+        pen = 6,
+        vel = 440
     },
 
     -- Magnum calibers
     ["357"] = {
         max = 50,
         min = 12,
-        pen = 9
+        pen = 9,
+        vel = 430
     },
     ["44"] = {
         max = 60,
         min = 15,
-        pen = 10
+        pen = 10,
+        vel = 360
     },
     ["50ae"] = {
         max = 70,
         min = 17,
-        pen = 12
+        pen = 12,
+        vel = 450
     },
     ["50beo"] = {
         max = 70,
         min = 20,
-        pen = 12
+        pen = 12,
+        vel = 540
     },
 
     -- Carbine/rifle calibers
     ["556"] = {
         max = 34,
         min = 20,
-        pen = 14
+        pen = 14,
+        vel = 915
     },
     ["300blk"] = {
         max = 40,
         min = 15,
-        pen = 10
+        pen = 10,
+        vel = 310
     },
     ["545"] = {
         max = 40,
         min = 20,
-        pen = 12
+        pen = 12,
+        vel = 880
     },
     ["762_39"] = { -- 7.62x39, not 7.62x51 NATO
         max = 50,
         min = 25,
-        pen = 16
+        pen = 16,
+        vel = 730
     },
     ["762_51"] = { -- 7.62x51 NATO
         max = 75,
         min = 40,
-        pen = 20
+        pen = 20,
+        vel = 850
     },
     ["366"] = { -- .366 TKM
         max = 60,
         min = 30,
-        pen = 18
+        pen = 18,
+        vel = 600
     },
 
     -- Shotgun calibers
     ["12g_p"] = { -- Pump
         max = 20,
         min = 13,
-        pen = 2
+        pen = 2,
+        vel = 400
     },
     ["12g_s"] = { -- Semi
         max = 18,
         min = 10,
-        pen = 2
+        pen = 2,
+        vel = 900
     },
     ["410b"] = {
         max = 18,
         min = 5,
-        pen = 2
+        pen = 2,
+        vel = 550
     },
 
     -- Sniper calibers
     ["338"] = {
         max = 85,
         min = 160,
-        pen = 36
+        pen = 36,
+        vel = 1000
     },
     ["300"] = {
         max = 44,
         min = 85,
-        pen = 24
+        pen = 24,
+        vel = 950
     },
     ["50bmg"] = {
         max = 104,
         min = 180,
-        pen = 46
+        pen = 46,
+        vel = 920
     },
 }
