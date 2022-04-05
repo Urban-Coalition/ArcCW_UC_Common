d0397f074eec5bc0af0d44859dc4f32541eef914
TheOnly8Z <888888zombies@gmail.com>
2022-04-05 02:14:26 -0400

    shotguns can't use comp. internals

--- lua/arccw/shared/attachments/uc_fg_civvy.lua
+++ lua/arccw/shared/attachments/uc_fg_civvy.lua
@@ -5,7 +5,6 @@ att.Description = "High quality, precision manufactured aftermarket parts that i
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
-    "uc.semionly"
 }
 att.Desc_Neutrals = {
 }
@@ -13,24 +12,16 @@ att.Slot = "uc_fg"
 att.AutoStats = true
 
 att.Hook_Compatible = function(wep)
-    if wep:GetIsManualAction() then
+    if wep:GetIsManualAction() or wep:GetIsShotgun() then
         return false
     end
 end
 
-att.Override_Firemodes = {
-    {
-        Mode = 1,
-    },
-    {
-        Mode = 0
-    }
-}
 att.Override_Firemodes_Priority = 10
 
 att.Mult_Range = 1.25
-att.Mult_AccuracyMOA = 0.5
-att.Mult_RPM = .8
+att.Mult_AccuracyMOA = 0.75
+att.Mult_RPM = 0.8
 att.Mult_MalfunctionMean = 1.5
 
 att.AttachSound = "arccw_uc/common/gunsmith/internal_modification.ogg"
