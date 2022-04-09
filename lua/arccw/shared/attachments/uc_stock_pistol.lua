caac2e8e53af651129b86fb78ecd9281df328821
Sninctbur <hedpub@protonmail.com>
2022-04-08 20:23:28 -0500

    Pistol stock

---
+++ lua/arccw/shared/attachments/uc_stock_pistol.lua
@@ -0,0 +1,13 @@
+att.PrintName = "FAB Defense Collapsible Tactical Stock"
+att.AbbrevName = "Pistol Stock"
+att.Icon = nil
+att.Description = "Mitigates the recoil disadvantage typical of most pistols, but extends the weapon's profile substantially.\n\nYou probably shouldn't let the feds know you have this."
+att.Slot = "go_stock_pistol_bt"
+att.AutoStats = true
+
+att.Model = "models/weapons/arccw/atts/stock_fab.mdl"
+
+att.Mult_Recoil = .75
+att.Mult_RecoilSide = .5
+att.Mult_SightTime = 1.4
+att.Add_BarrelLength = 20
