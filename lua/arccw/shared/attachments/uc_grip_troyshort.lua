7d13927b615d022a7b41754cc618fadd2cd012d9
Talon141 <zachlaxtonbiz@gmail.com>
2022-04-25 15:21:19 -0700

    troy short modla replacement
    revert if its too nasty :frowning:

--- lua/arccw/shared/attachments/uc_grip_troyshort.lua
+++ lua/arccw/shared/attachments/uc_grip_troyshort.lua
@@ -1,15 +1,16 @@
-att.PrintName = "Troy Modular Combat Grip (Stubby)"
+att.PrintName = "BCM Mod. 3"
 att.AbbrevName = "Stubby Foregrip"
 att.Icon = Material("entities/att/shorttroy.png", "mips smooth")
 att.Description = "Decreases recoil by providing a more natural forearm position."
 
 att.AutoStats = true
 att.Slot = "foregrip"
+att.SortOrder = 1
 
 att.LHIK = true
 
-att.ModelOffset = Vector(0, 0, -0.7)
-att.Model = "models/weapons/arccw/atts/uc_troygrip_short.mdl"
+att.ModelOffset = Vector(0, 0, -0.14)
+att.Model = "models/weapons/arccw/atts/ud_foregrip_mod3.mdl"
 -- att.ModelSkin  = 1
 
 att.Mult_Recoil = .8
