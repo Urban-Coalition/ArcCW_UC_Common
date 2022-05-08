502ce0fff9dd2f512f283fcb1c88a4ef9c7ae88f
Fesiug <publicfesiug@outlook.com>
2022-05-08 19:28:03 -0400

    📓

--- lua/arccw/shared/attachments/uc_charm_sgmanual.lua
+++ lua/arccw/shared/attachments/uc_charm_sgmanual.lua
@@ -2,7 +2,7 @@ att.PrintName = "Slugger"
 
 att.SortOrder = 1
 att.Icon = Material("entities/att/arccw_uc_ammo_shotgun_generic.png", "mips smooth")
-att.Description = "Forces weapons to only use manual-action modes. Not the most competitively viable, but it surely means you can still kick ass without all those fancy-shmancy automatic modes."
+att.Description = "Forces weapons to only use manual-action modes. Not the most competitively viable, but it surely shows you can still kick ass without all those fancy-shmancy automatic modes."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
