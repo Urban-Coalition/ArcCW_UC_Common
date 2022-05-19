2ac8400cf6fdee72ee1a37edd9ab22ac949a2b56
Sninctbur <hedpub@protonmail.com>
2022-05-19 14:58:59 -0500

    Blank grammar change

--- lua/arccw/shared/attachments/uc_ammo_blank.lua
+++ lua/arccw/shared/attachments/uc_ammo_blank.lua
@@ -2,7 +2,7 @@ att.PrintName = "\"BLNK\" Blank Cartridges"
 att.AbbrevName = "\"BLNK\" Blanks"
 att.SortOrder = -2
 att.Icon = Material("entities/att/arccw_uc_ammo_blank.png", "mips smooth")
-att.Description = [[Cartridges sealed without a solid projectile, but otherwise performs identically. Used when the muzzle report of a gunshot is necessary without the bullet, such as in filming, ceremonies, sports, and combat training.
+att.Description = [[Cartridges sealed without a solid projectile. Performance is otherwise identical. Used when the muzzle report of a gunshot is necessary without the bullet, such as in filming, ceremonies, sports, and combat training.
 
 In real life, blanks are not harmless. The muzzle shockwave or discharged wadding can and have killed people who do not exercise standard firearm precautions.]]
 att.Desc_Pros = {
