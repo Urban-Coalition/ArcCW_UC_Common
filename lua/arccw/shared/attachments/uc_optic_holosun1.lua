2e3637a0c2e5a92be29d7102f090b252e5a245a5
Fesiug <publicfesiug@outlook.com>
2022-07-02 00:59:48 -0700

    Holosun! and riser variant

---
+++ lua/arccw/shared/attachments/uc_optic_holosun1.lua
@@ -0,0 +1,35 @@
+att.PrintName = "Holosun (Riser) (RDS)"
+att.AbbrevName = "Holosun (R) (RDS)"
+att.Icon = Material("entities/att/t1.png", "mips smooth")
+att.Description = "Improves sighted visibility with a red-dot targeting reticle, while adding minimal extra weight."
+
+att.SortOrder = 1
+
+att.Desc_Pros = {
+    "autostat.holosight",
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = {"optic"}
+
+att.Model = "models/weapons/arccw/atts/uc_holosun1.mdl"
+att.ModelScale = Vector( 1.5, 1.5, 1.5 )
+
+att.AdditionalSights = {
+    {
+        Pos = Vector(0, 3, -1.5),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.1,
+        ScrollFunc = ArcCW.SCROLL_NONE
+    }
+}
+
+att.Holosight = true
+att.HolosightReticle = Material("hud/reticles/uc_reddot.png", "mips smooth")
+att.HolosightNoFlare = false
+att.HolosightSize = 1.5
+att.HolosightBone = "holosight"
+att.Colorable = true
+
+att.Mult_SightedSpeedMult = .9
