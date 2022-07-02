988cbcd7b773c60c0ae70fb5cc673a9e89574229
TheOnly8Z <888888zombies@gmail.com>
2022-07-01 20:21:43 -0500

    compm2

---
+++ lua/arccw/shared/attachments/uc_optic_comp_m2.lua
@@ -0,0 +1,37 @@
+att.PrintName = "AIMPOINT CompM2 Red Dot (RDS)"
+att.AbbrevName = "CompM2 (RDS)"
+att.Icon = Material("entities/att/t1.png", "mips smooth")
+att.Description = "Improves sighted visibility with a red-dot targeting reticle, while adding minimal extra weight."
+
+att.SortOrder = 1
+
+att.Desc_Pros = {
+    "autostat.holosight",
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = {"optic"}
+
+att.Model = "models/weapons/arccw/atts/uc_comp_m2.mdl"
+att.ModelOffset = Vector(0, 0, 0)
+att.ModelScale = Vector(0.9, 0.9, 0.9)
+
+att.AdditionalSights = {
+    {
+        Pos = Vector(0, 3, -1.5), -- -1.44
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.1,
+        ScrollFunc = ArcCW.SCROLL_NONE
+    }
+}
+
+att.Holosight = true
+att.HolosightReticle = Material("hud/reticles/uc_reddot.png", "mips smooth")
+att.HolosightPiece = "models/weapons/arccw/atts/uc_comp_m2_hsp.mdl"
+att.HolosightNoFlare = true
+att.HolosightSize = 4
+att.HolosightBone = "holosight"
+att.Colorable = true
+
+att.Mult_SightedSpeedMult = .9
