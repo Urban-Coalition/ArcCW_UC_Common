42a6f06ea1ad767288ded4d1140cc899a89b3f0b
AurieeDumDum <83324542+AurieeDumDum@users.noreply.github.com>
2022-07-03 17:26:19 -0500

    Forgip rebalancss
    hello dr freeman

--- lua/arccw/shared/attachments/uc_grip_troy.lua
+++ lua/arccw/shared/attachments/uc_grip_troy.lua
@@ -1,12 +1,12 @@
-att.PrintName = "Troy Modular Combat Grip"
-att.AbbrevName = "Long Foregrip"
+att.PrintName = "Troy Modular Com(ically large)bat Grip"
+att.AbbrevName = "fatass foregrip"
 att.Icon = Material("entities/att/longtroy.png", "mips smooth")
-att.Description = "Long, massive foregrip tailored for exquisite weapon control. Makes the weapon impossible to conceal well."
+att.Description = "Long, dummy stupid foregrip tailored for maximum weapon control. Makes the weapon impossible to conceal well. Urban Woodchipper has taken this grip to the Congo"
 
 att.AutoStats = true
 att.Slot = "foregrip"
 
-att.LHIK = true
+att.LHIK = false
 
 att.ModelOffset = Vector(0, 0, -0.7)
 att.Model = "models/weapons/arccw/atts/uc_troygrip.mdl"
@@ -16,9 +16,12 @@ att.Mult_Recoil = .05
 att.Mult_HipDispersion = .1
 att.Mult_SightTime = 5
 att.Mult_DrawTime = 2.5
-att.Mult_ReloadTime = 2.5
+att.Mult_ReloadTime = 1.75
+att.Mult_MoveSpeed = .5
+att.Mult_Sway = .05
 
-att.Override_HoldtypeActive = "smg"
+att.Override_HoldtypeActive = "revolver"
 att.Override_HoldtypeActive_Priority = 2
 
--- att.Ignore = true
+att.Ignore = true
+
