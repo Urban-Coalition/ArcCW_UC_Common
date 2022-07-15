c89d83293ed52808a3200a3644b21d6456004267
Fesiug <publicfesiug@outlook.com>
2022-07-14 21:08:13 -0400

    his name is Retarded and he was born without limbs

--- lua/arccw/shared/attachments/uc_muzzle_supressor_longass.lua
+++ lua/arccw/shared/attachments/uc_muzzle_supressor_longass.lua
@@ -10,8 +10,8 @@ att.Slot = {"muzzle"}
 att.SortOrder = 150
 
 att.Model = "models/weapons/arccw/atts/uc_longass_silencer.mdl"
-att.ModelOffset = Vector(2, 0, -0.05)
-att.ModelScale = Vector(1.15, 1.15, 1.15)
+att.ModelOffset = Vector(1.5, 0, 0.00)
+att.ModelScale = Vector(1, 1.25, 1.25)
 att.OffsetAng = Angle(0, 0, 0)
 
 att.Silencer = true
