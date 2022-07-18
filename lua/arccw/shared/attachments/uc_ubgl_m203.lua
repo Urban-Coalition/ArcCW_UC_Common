5856645f22e1017d1f50732334602fb2a0285dac
Fesiug <publicfesiug@outlook.com>
2022-07-18 18:36:13 -0400

    M203!!

---
+++ lua/arccw/shared/attachments/uc_ubgl_m203.lua
@@ -0,0 +1,14 @@
+att.PrintName = "M203 Underbarrel Greande Launcher"
+att.AbbrevName = "M203 UBGL"
+att.Icon = Material("entities/att/shorttroy.png", "mips smooth")
+att.Description = "AAAAAAAAAAAAAAA GANGS SHIT STRIAGHT KILLA.>!~!!!!~!~!~ PRA PRA PRA PRA"
+
+att.SortOrder = -100000
+
+att.AutoStats = true
+att.Slot = "ubgl"
+
+att.LHIK = true
+
+att.ModelOffset = Vector(0, 0, 0)
+att.Model = "models/weapons/arccw/atts/uc_ubgl_m203_v1.mdl"
