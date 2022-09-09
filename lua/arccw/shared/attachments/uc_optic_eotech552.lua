f174d1b7891935af3fd0a37c739547f6ffcd68ec
Taskmer <93636955+Taskmer@users.noreply.github.com>
2022-09-10 09:58:41 +1000

    Eotech reticle centered and shrinked

--- lua/arccw/shared/attachments/uc_optic_eotech552.lua
+++ lua/arccw/shared/attachments/uc_optic_eotech552.lua
@@ -18,7 +18,7 @@ att.ModelScale = Vector(0.67, 0.67, 0.67)
 
 att.AdditionalSights = {
     {
-        Pos = Vector(0, 9, -1.35),
+        Pos = Vector(0, 9, -1.38),
         Ang = Angle(0, 0, 0),
         Magnification = 1.1,
         ScrollFunc = ArcCW.SCROLL_NONE
@@ -27,7 +27,7 @@ att.AdditionalSights = {
 
 att.Holosight = true
 att.HolosightReticle = Material("hud/reticles/ud_holo.png", "smooth")
-att.HolosightSize = 1.3
+att.HolosightSize = 1.1
 att.HolosightBone = "holosight"
 att.HolosightNoFlare = true
 
