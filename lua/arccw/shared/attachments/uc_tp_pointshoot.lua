5529457fdfe0614f0b16abc9122ad4778273a04e
Fesiug <publicfesiug@outlook.com>
2022-12-20 20:48:34 -0500

    starting

--- lua/arccw/shared/attachments/uc_tp_pointshoot.lua
+++ lua/arccw/shared/attachments/uc_tp_pointshoot.lua
@@ -1,6 +1,6 @@
 att.PrintName = "Point Shooting"
 
-att.Icon = Material("entities/att/arccw_uc_tp_point_shooting.png", "smooth mips")
+att.Icon = Material("entities/att/arccw_uc_tp_pointshooting.png", "smooth mips")
 att.Description = "Dedicated practice at shooting without using the sights has given you the ability to fire from the hip more effectively. If John Rambo can do it, so can you."
 att.Desc_Pros = {
 }
@@ -13,4 +13,4 @@ att.Slot = "uc_tp"
 att.AutoStats = true
 att.SortOrder = 6
 
-att.Mult_HipDispersion = .75
+att.Mult_HipDispersion = 0.4
