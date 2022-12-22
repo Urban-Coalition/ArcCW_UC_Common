0cafcd86c9234c020dad7bfda908f132cabbe6ce
Fesiug <publicfesiug@outlook.com>
2022-12-22 15:27:45 -0500

    the optical gunsight .

---
+++ lua/arccw/shared/attachments/uc_optic_elcan.lua
@@ -0,0 +1,55 @@
+att.PrintName = "Trijicon Advanced Combat Optical Gunsight (4x)"
+att.AbbrevName = "ACOG (4x)"
+att.Icon = Material("entities/att/acwatt_uc_optic_acog.png", "mips smooth")
+att.Description = "Medium range combat scope for improved precision at longer ranges.\nEquipped with backup iron sights for use in emergencies."
+
+att.SortOrder = 4
+
+att.Desc_Pros = {
+    "autostat.holosight",
+    "autostat.zoom",
+}
+att.Desc_Neutrals = {"uc.bus"}
+
+att.AutoStats = true
+att.Slot = {"optic", "ud_optic", "ud_acog"}
+
+att.Model = "models/weapons/arccw/atts/ud_acog.mdl"
+att.ModelOffset = Vector(0, 0, 0)
+att.ModelScale = Vector(1.15, 1.15, 1.15)
+att.OffsetAng = Angle(0, 0, 0)
+
+att.AdditionalSights = {
+    {
+        Pos = Vector(0, 8, -1.48),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.25,
+        IgnoreExtra = true,
+        CrosshairInSights = false,
+        HolosightBone = "holosight",
+        HolosightData = {
+            Holosight = true,
+            HolosightReticle = Material("hud/scopes/uc_acog_reticle.png", "smooth"),
+            HolosightNoFlare = true,
+            HolosightSize = 9.5,
+            HolosightPiece = "models/weapons/arccw/atts/ud_acog_hsp.mdl",
+            HolosightBlackbox = true,
+            HolosightMagnification = 4,
+            Colorable = true,
+        },
+    },
+    {
+        Pos = Vector(-0.005, 11, -2.632),
+        Ang = Angle(-1, 0, 0),
+        Magnification = 1.1,
+        IgnoreExtra = true,
+        HolosightData = {
+            Holosight = false,
+        },
+    },
+}
+
+att.Colorable = true
+att.HolosightPiece = "models/weapons/arccw/atts/ud_acog_hsp.mdl"
+
+att.Mult_SightedSpeedMult = 0.75
---
+++ lua/arccw/shared/attachments/uc_optic_hamr.lua
@@ -0,0 +1,58 @@
+att.PrintName = "Leupold Mark 4 High Accuracy Multi-Range Riflescope (3x/HOLO)"
+att.AbbrevName = "HAMR (3x/HOLO)"
+att.Icon = Material("entities/att/acwatt_uc_optic_acog.png", "mips smooth")
+att.Description = "Medium range scope with integrated DeltaPoint reflex sight."
+
+att.SortOrder = 4
+
+att.Desc_Pros = {
+    "autostat.holosight",
+    "autostat.zoom",
+}
+att.Desc_Neutrals = {"uc.bus"}
+
+att.AutoStats = true
+att.Slot = {"optic", "ud_optic", "ud_acog"}
+
+att.Model = "models/weapons/arccw/atts/uc_gso_hamr.mdl"
+att.ModelOffset = Vector(0, 0, 0)
+att.ModelScale = Vector(1, 1, 1)
+att.OffsetAng = Angle(0, 0, 0)
+
+att.AdditionalSights = {
+    {
+        Pos = Vector(0, 8, -1.56325),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.25,
+        IgnoreExtra = true,
+        CrosshairInSights = false,
+        HolosightBone = "holosight",
+        HolosightData = {
+            Holosight = true,
+            HolosightReticle = Material("hud/scopes/uc_hamr.png", "smooth"),
+            HolosightNoFlare = true,
+            HolosightSize = 9.5,
+            HolosightPiece = "models/weapons/arccw/atts/uc_gso_hamr_hsp.mdl",
+            HolosightBlackbox = true,
+            HolosightMagnification = 3,
+            Colorable = true,
+        },
+    },
+    {
+        Pos = Vector(0, 8, -2.94738),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.1,
+        IgnoreExtra = true,
+        HolosightData = {
+            Holosight = true,
+            HolosightReticle = Material("hud/reticles/uc_reddot.png", "mips smooth"),
+            HolosightSize = 2,
+            Colorable = true,
+        },
+    },
+}
+
+att.Colorable = true
+att.HolosightPiece = "models/weapons/arccw/atts/uc_gso_hamr_hsp.mdl"
+
+att.Mult_SightedSpeedMult = 0.75
