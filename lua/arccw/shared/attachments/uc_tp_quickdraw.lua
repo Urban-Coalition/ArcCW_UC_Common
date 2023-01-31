8076b85379fd9fe4beacdd3d0b7f4ba4ba1367e8
Fesiug <publicfesiug@outlook.com>
2023-01-31 17:42:54 -0500

    Combine Quick Draw and Snapshot

--- lua/arccw/shared/attachments/uc_tp_quickdraw.lua
+++ lua/arccw/shared/attachments/uc_tp_quickdraw.lua
@@ -1,7 +1,7 @@
 att.PrintName = "Quick Draw"
 
 att.Icon = Material("entities/att/arccw_uc_tp_quickdraw.png", "smooth mips")
-att.Description = "Like the fastest guns in the Old West, you can draw and shoot in the blink of an eye.\n\nTexas Red had not cleared leather 'fore a bullet fairly ripped, and the Ranger's aim was deadly with the big iron on his hip."
+att.Description = "Like the fastest guns in the Old West, you can draw, aim, and shoot in the blink of an eye.\n\nTexas Red had not cleared leather 'fore a bullet fairly ripped, and the Ranger's aim was deadly with the big iron on his hip."
 att.Desc_Pros = {
 }
 att.Desc_Cons = {
@@ -14,5 +14,6 @@ att.AutoStats = true
 att.SortOrder = 5
 
 att.Mult_DrawTime = 0.5
+att.Mult_SightTime = 0.85
 
 att.NotForNPCs = true
